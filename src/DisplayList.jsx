908523ad59218ec7e252fa24e0bc68a7fa280503
Joe Waddicor <joewaddicor@Joes-MacBook-Pro.local>
2021-10-07 15:43:51 +0100

    Added table functionality to list

--- src/DisplayList.jsx
+++ src/DisplayList.jsx
@@ -1,9 +1,31 @@
 function DisplayList ({list}) {
     return (
     <section className="display-list">
-        <ul>{list.map((task) => {
+        <table className="task-table">
+            <thead>
+            <tr>
+                <th>Task</th>
+                <th>Completed</th>
+            </tr>
+            </thead>
+            <tbody>
+            {list.map((task) => {
+                if (task.completed) {
+                    return(<tr key={task.name}><td>{task.name}</td>
+                        <td>✅</td>
+                      </tr> 
+                    )
+                } else {
+                    return (<tr key={task.name}><td>{task.name}</td>
+                        <td>❌</td>
+                      </tr>  )
+                }
+            })}
+            </tbody>
+        </table>
+        {/* <ul>{list.map((task) => {
             return <li key={task.name}>{task.name}</li>
-        })}</ul>
+        })}</ul> */}
     </section>
 
     )

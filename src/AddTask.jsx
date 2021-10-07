64f37bda0e914f47acd2b02f0d84a050dc2c8dce
Joe Waddicor <joewaddicor@Joes-MacBook-Pro.local>
2021-10-07 15:17:19 +0100

    Added handleSubmit functionality to list

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -1,9 +1,23 @@
-function AddTask () {
+import {useState} from 'react';
+
+function AddTask ({setList}) {
+    const [newTask, setNewTask] = useState('')
+    function handleSubmit(e) {
+        e.preventDefault();
+        setList((currList) => {
+            const taskToAdd = {name: newTask, completed: false}
+            const updatedList = [...currList, taskToAdd];
+            return updatedList;
+        })
+        setNewTask('')
+    }
     return (
     <section className="add-task">
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="list-addition">Add to the list</label>
-            <input type="text" id="list-addition"/>
+            <input type="text" required value={newTask} onChange={(e) => {
+                setNewTask(e.target.value)
+            }} id="list-addition"/>
             <button>Submit Task</button>
         </form>
     </section>

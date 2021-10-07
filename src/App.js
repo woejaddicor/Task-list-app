7cc253f41efcab476cb3a624624ed6cc26c11120
Joe Waddicor <joewaddicor@Joes-MacBook-Pro.local>
2021-10-07 14:38:13 +0100

    Added list state to app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import AddTask from './AddTask';
 import './App.css';
 import DisplayList from './DisplayList';
 import Header from './Header';
+import {useState} from 'react';
 
 function App() {
+  const [list, setList] = useState([
+    {name: 'Wash the car', completed: false},
+    {name: 'Do the dishes', completed: false},
+    {name: 'Feed the dog', completed: true}
+  ])
   return (
    <div className="app"> 
    <Header />,
-   <AddTask />,
-   <DisplayList />
+   <AddTask setList={setList}/>,
+   <DisplayList list={list}/>
    </div>
   );
 }
